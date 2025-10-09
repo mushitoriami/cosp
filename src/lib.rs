054291d84342399a9e2347ebbf20650194cad70d
Komorebi Sakuramochi <komorebi_sakuramochi@icloud.com>
2025-10-09 04:02:01 +0000

    Replace test_take_term with test_parse_term

--- src/lib.rs
+++ src/lib.rs
@@ -54,120 +54,6 @@ impl Parser {
 mod tests {
     use super::*;
 
-    fn test_take_term_success(input: &str, output: Term, remain: Vec<&str>) {
-        let mut parser = Parser::new();
-        let mut iter = parser.tokenizer.tokenize(input).map_while(|x| x.ok());
-        let term = Parser::take_term(&mut iter);
-        assert_eq!(term, Some(output));
-        assert_eq!(iter.collect::<Vec<&str>>(), remain);
-    }
-
-    fn test_take_term_fail(input: &str) {
-        let mut parser = Parser::new();
-        let mut iter = parser.tokenizer.tokenize(input).map_while(|x| x.ok());
-        let term = Parser::take_term(&mut iter);
-        assert_eq!(term, None);
-    }
-
-    #[test]
-    fn test_take_term_1() {
-        test_take_term_success(
-            "ab(c_d(e_f*),g_h?)))(",
-            Term::Compound(
-                String::from("ab"),
-                vec![
-                    Term::Compound(
-                        String::from("c_d"),
-                        vec![Term::Constant(String::from("e_f"))],
-                    ),
-                    Term::Variable(String::from("g_h")),
-                ],
-            ),
-            vec![")", ")", "("],
-        );
-    }
-
-    #[test]
-    fn test_take_term_2() {
-        test_take_term_fail("ab(c_d(*e_f),?g_h)))(");
-    }
-
-    #[test]
-    fn test_take_term_3() {
-        test_take_term_success("a*(", Term::Constant(String::from("a")), vec!["("]);
-    }
-
-    #[test]
-    fn test_take_term_4() {
-        test_take_term_fail("a,a*)");
-    }
-
-    #[test]
-    fn test_take_term_5() {
-        test_take_term_fail("a(b*(c*)");
-    }
-
-    #[test]
-    fn test_take_term_6() {
-        test_take_term_fail("a)");
-    }
-
-    #[test]
-    fn test_take_term_7() {
-        test_take_term_fail("a(b**");
-    }
-
-    #[test]
-    fn test_take_term_8() {
-        test_take_term_fail("a(*)");
-    }
-
-    #[test]
-    fn test_take_term_9() {
-        test_take_term_fail("(a*)");
-    }
-
-    #[test]
-    fn test_take_term_10() {
-        test_take_term_success("a*a", Term::Constant(String::from("a")), vec!["a"]);
-    }
-
-    #[test]
-    fn test_take_term_11() {
-        test_take_term_fail("a(a*a)");
-    }
-
-    #[test]
-    fn test_take_term_12() {
-        test_take_term_success(
-            "f(a*, b*, x?)",
-            Term::Compound(
-                String::from("f"),
-                vec![
-                    Term::Constant(String::from("a")),
-                    Term::Constant(String::from("b")),
-                    Term::Variable(String::from("x")),
-                ],
-            ),
-            vec![],
-        );
-    }
-
-    #[test]
-    fn test_take_term_13() {
-        test_take_term_fail("f(a, b, X)");
-    }
-
-    #[test]
-    fn test_take_term_14() {
-        test_take_term_fail("f(*a, *b, ?x)");
-    }
-
-    #[test]
-    fn test_take_term_15() {
-        test_take_term_fail("ab(c_d(e_f),g_h)))(");
-    }
-
     fn test_parse_term_success(input: &str, output: Term) {
         let mut parser = Parser::new();
         let term = parser.parse_term(input);
@@ -183,6 +69,73 @@ mod tests {
     #[test]
     fn test_parse_term_1() {
         test_parse_term_success(
+            "ab(c_d(e_f*),g_h?)",
+            Term::Compound(
+                String::from("ab"),
+                vec![
+                    Term::Compound(
+                        String::from("c_d"),
+                        vec![Term::Constant(String::from("e_f"))],
+                    ),
+                    Term::Variable(String::from("g_h")),
+                ],
+            ),
+        );
+    }
+
+    #[test]
+    fn test_parse_term_2() {
+        test_parse_term_fail("ab(c_d(*e_f),?g_h)))(");
+    }
+
+    #[test]
+    fn test_parse_term_3() {
+        test_parse_term_fail("a*(");
+    }
+
+    #[test]
+    fn test_parse_term_4() {
+        test_parse_term_fail("a,a*)");
+    }
+
+    #[test]
+    fn test_parse_term_5() {
+        test_parse_term_fail("a(b*(c*)");
+    }
+
+    #[test]
+    fn test_parse_term_6() {
+        test_parse_term_fail("a)");
+    }
+
+    #[test]
+    fn test_parse_term_7() {
+        test_parse_term_fail("a(b**");
+    }
+
+    #[test]
+    fn test_parse_term_8() {
+        test_parse_term_fail("a(*)");
+    }
+
+    #[test]
+    fn test_parse_term_9() {
+        test_parse_term_fail("(a*)");
+    }
+
+    #[test]
+    fn test_parse_term_10() {
+        test_parse_term_fail("a*a");
+    }
+
+    #[test]
+    fn test_parse_term_11() {
+        test_parse_term_fail("a(a*a)");
+    }
+
+    #[test]
+    fn test_parse_term_12() {
+        test_parse_term_success(
             "f(a*, b*, x?)",
             Term::Compound(
                 String::from("f"),
@@ -196,7 +149,42 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_term_2() {
+    fn test_parse_term_13() {
+        test_parse_term_fail("f(a, b, X)");
+    }
+
+    #[test]
+    fn test_parse_term_14() {
+        test_parse_term_fail("f(*a, *b, ?x)");
+    }
+
+    #[test]
+    fn test_parse_term_15() {
+        test_parse_term_fail("ab(c_d(e_f),g_h)))(");
+    }
+
+    #[test]
+    fn test_parse_term_16() {
+        test_parse_term_fail("ab(c_d(e_f*),g_h?)))(");
+    }
+
+    #[test]
+    fn test_parse_term_17() {
+        test_parse_term_success(
+            "f(a*, b*, x?)",
+            Term::Compound(
+                String::from("f"),
+                vec![
+                    Term::Constant(String::from("a")),
+                    Term::Constant(String::from("b")),
+                    Term::Variable(String::from("x")),
+                ],
+            ),
+        );
+    }
+
+    #[test]
+    fn test_parse_term_18() {
         test_parse_term_fail("ab(c_d(e_f),g_h)))(");
     }
 }
